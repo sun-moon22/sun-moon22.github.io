95b1cadf9b02cfcec9446b2867dd21cd7293717a
sun_moon <1476380153@qq.com>
2022-04-20 12:03:44 +0800

    Site updated: 2022-04-20 12:03:37

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "5",
+      "tmpColor": "4A86E8",
+      "tmpSize": "16",
+      "cityColor": "FF9900",
+      "citySize": "16",
+      "aqiColor": "4A86E8",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "0px 0px 0px 0px",
+      "shadow": "0",
+      "language": "auto",
+      "fixed": "true",
+      "vertical": "center",
+      "horizontal": "center",
+      "left": "200",
+      "top": "20",
+      "key": "0b2cbb9fabb84744a6fdd32884b53eb6"
+    }
+  }
